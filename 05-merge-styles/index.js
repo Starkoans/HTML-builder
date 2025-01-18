0b3adfced6ffaabdc4901af29d42fbf0534e0d53
Anastasiia <starkoans@gmail.com>
2025-01-18 23:45:40 +0500

    fix: 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,10 +2,12 @@ const fs = require('node:fs/promises');
 const path = require('node:path');
 
 const stylesDir = path.join(__dirname, 'styles');
-const bundlePath = path.join(stylesDir, 'bundle.css');
+const projectDist = path.join(__dirname, 'project-dist');
+const bundlePath = path.join(projectDist, 'bundle.css');
 
 (async function () {
     try {
+
         const files = await fs.readdir(stylesDir, { withFileTypes: true });
         const cssFiles = files.filter((file) => path.extname(file.name) === '.css');
     
