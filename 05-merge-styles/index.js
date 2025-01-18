55e4836db89969c648a698aa828be8a71b324487
Anastasiia <starkoans@gmail.com>
2025-01-18 23:35:25 +0500

    feat: 5/6 done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const stylesDir = path.join(__dirname, 'styles');
+const bundlePath = path.join(stylesDir, 'bundle.css');
+
+(async function () {
+    try {
+        const files = await fs.readdir(stylesDir, { withFileTypes: true });
+        const cssFiles = files.filter((file) => path.extname(file.name) === '.css');
+    
+    
+        const arr = [];
+        for (const file of cssFiles) {
+            const filePath = path.join(stylesDir, file.name);
+            const fileData = await fs.readFile(filePath, 'utf-8');
+            arr.push(fileData);
+        }
+    
+        const mergedStyles = arr.join('\n');
+    
+        await fs.writeFile(bundlePath, mergedStyles, 'utf-8');
+    } catch (error) {
+        console.log(error)
+    }
+
+})()
