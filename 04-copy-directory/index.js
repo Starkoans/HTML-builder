80f9a3e74575956cda44b03b50af4a4ae9e725aa
Anastasiia <starkoans@gmail.com>
2025-01-19 20:47:58 +0500

    refactor: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,30 @@
-const { copyFile, readdir, mkdir } = require('node:fs/promises');
+const fs  = require('node:fs/promises');
 const path = require('node:path');
 
-async function copyDir() {
+async function copyDir(srcDirPath, destDirPath) {
     try {
-        const srcDirPath = path.join(__dirname, 'files');
-        const destDirPath = path.join(__dirname, 'files-copy');
-        
-        await mkdir(destDirPath, { recursive: true });
+        await fs.mkdir(destDirPath, { recursive: true });
 
-        const filesToCopy = await readdir(srcDirPath);
+        const items = await fs.readdir(srcDirPath, { withFileTypes: true });
 
-        for (const file of filesToCopy) {
-            copyFile(path.join(srcDirPath, file), path.join(destDirPath, file));
+        for (const item of items) {
+            const srcItem = path.join(srcDirPath, item.name);
+            const destItem = path.join(destDirPath, item.name);
+
+            if (item.isDirectory()) {
+                await copyDir(srcItem, destItem);
+            } else {
+                await fs.copyFile(srcItem, destItem);
+            }
         }
     } catch (error) {
-        console.log(error)
+        console.error('Error copying directory:', error);
     }
-
 }
 
-copyDir();
+const srcDirPath = path.join(__dirname, 'files');
+const destDirPath = path.join(__dirname, 'files-copy');
+
+copyDir(srcDirPath, destDirPath);
+
+module.exports = { copyDir };
